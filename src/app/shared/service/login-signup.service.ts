b2818b83e11b93888a757b99a6fb5dc25ef74624
merlin <merlinsimon98@gmail.com>
2021-12-02 22:43:16 +0100

    tiens

--- src/app/shared/service/login-signup.service.ts
+++ src/app/shared/service/login-signup.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { User } from '../model/User.model';
+import * as Speakeasy from 'speakeasy';
 
 @Injectable({
   providedIn: 'root'
@@ -15,20 +16,32 @@ export class LoginSignupService {
     }else{
       this.isAuthenticated$.next(false);
     }
+    if(localStorage.getItem('user')){
+      console.warn("SOUMISSION")
+      console.warn(JSON.parse(localStorage.getItem('user')!))
+      this.user$.next(JSON.parse(localStorage.getItem('user')!))
+    }
   }
 
   key = 'users';
 
   isAuthenticated$ = new BehaviorSubject(false);
+  user$: Subject<User> = new Subject();
 
   signUp(user: User): void{
+    user.secret = Speakeasy.generateSecret();
+
     if(localStorage.getItem(this.key)){
-      let users: User[] = JSON.parse(localStorage.getItem(this.key)!).concat(user);
+      let users: User[] = JSON.parse(localStorage.getItem(this.key)!)
+      if(users.some(u => u.username===user.username && u.password===user.password)){
+        this.snackBar.open('already signup', '', {duration: 2000});
+      }
+      users.concat(user);
       localStorage.setItem(this.key, JSON.stringify(users));
     }else{
       localStorage.setItem(this.key, JSON.stringify([user]));
     }
-    this.router.navigate(['login'])
+    this.router.navigate(['qrcode'])
     this.snackBar.open('signup success', '', {duration: 2000});
   }
 
@@ -36,9 +49,11 @@ export class LoginSignupService {
     if(localStorage.getItem(this.key)){
       let users: User[] = JSON.parse(localStorage.getItem(this.key)!);
       this.isAuthenticated$.next(true);
-      let ok = users.some(u => u.username===user.username && u.password===user.password);
-      if(ok){
+      let userFind = users.find(u => u.username===user.username && u.password===user.password);
+      if(!!userFind){
+        this.user$.next(userFind);
         localStorage.setItem('logged', 'true');
+        localStorage.setItem('user', JSON.stringify(userFind));
         this.snackBar.open('signin success', '', {duration: 2000});
         this.router.navigate(['authenticated'])
         return true;
@@ -53,8 +68,10 @@ export class LoginSignupService {
   }
 
   logout(): void{
+    this.user$.next(undefined);
     this.isAuthenticated$.next(false);
     localStorage.setItem('logged', 'false');
+    localStorage.removeItem('user');
     this.router.navigate(['login'])
     this.snackBar.open('logout success', '', {duration: 2000});
   }
